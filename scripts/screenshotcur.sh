4c1382f25e36ef474df5badb4c7d4e9f0dbd7b7c
Omid <147955568+MrAventador@users.noreply.github.com>
2023-12-15 14:43:09 +0330

    Add files via upload

--- scripts/screenshotcur.sh
+++ scripts/screenshotcur.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mkdir -p /home/omid/Pictures/Screenshots && scrot /home/omid/Pictures/Screenshots/%m-%d-%Y-%H%M%S.png -u
+mkdir -p /home/omid/Pictures/Screenshots && scrot /home/omid/Pictures/Screenshots/%m-%d-%Y-%H%M%S.png -u -e 'xclip -selection clipboard -t image/png -i $f'
